a4d2015e2d740b6276e66d5e1b003944ac363c56
Steven Capuzzi <stevenczz@gmail.com>
2019-02-05 13:32:03 -0500

    schema modification

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_02_05_164103) do
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
