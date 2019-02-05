f4797eace6a48dd0fc14eaa35cbe0c9d2f11e672
Steven Capuzzi <stevenczz@gmail.com>
2019-02-05 11:38:50 -0500

    Creates Admin table

---
+++ app/views/admins/_admin.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! admin, :id, :name, :email, :created_at, :updated_at
+json.url admin_url(admin, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_162254) do
+ActiveRecord::Schema.define(version: 2019_02_05_163652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "applicants", force: :cascade do |t|
     t.string "name"
