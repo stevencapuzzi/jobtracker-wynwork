fadf6e299646509b732a6e27387aa5bb36cf0b85
Steven Capuzzi <stevenczz@gmail.com>
2019-02-05 13:19:48 -0500

    adds password option to create new admin

--- db/migrate/20190205163652_create_admins.rb
+++ db/migrate/20190205163652_create_admins.rb
@@ -3,6 +3,7 @@ class CreateAdmins < ActiveRecord::Migration[5.2]
     create_table :admins do |t|
       t.string :name
       t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
